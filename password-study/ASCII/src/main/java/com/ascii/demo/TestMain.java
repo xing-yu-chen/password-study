6aa7fcd70cda60c88f723d7a4fa64f5ac97ec480
xing-yu-chen <17367288870@sina.cn>
2022-02-07 12:41:08 +0800

    ascii

---
+++ password-study/ASCII/src/main/java/com/ascii/demo/TestMain.java
@@ -0,0 +1,14 @@
+package com.ascii.demo;
+
+/**
+ * @Author: xing-yu-chen
+ * @Project: password-study
+ * @Description: 字符转换成ASCII码
+ * @Data:Created in 2022/2/6 10:24 下午
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        char a =  'A';
+        
+    }
+}
